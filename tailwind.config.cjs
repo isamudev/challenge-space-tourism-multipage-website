b76fdd97271501628c69a4e5faab0833a0211fb0
Isamu <support@isamu.dev>
2022-10-14 18:33:30 -0300

    feat: vscode settings, layout & fonts define and tailwindcss theme extend

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,20 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        default: '#D0D6F9',
+        primary: '#0B0D17',
+      },
+      fontFamily: {
+        barlow: ['Barlow', ...defaultTheme.fontFamily.sans],
+        'barlow-condensed': ['Barlow Condensed', ...defaultTheme.fontFamily.sans],
+        bellefair: ['Bellefair', ...defaultTheme.fontFamily.serif],
+      },
+    },
+  },
+  plugins: [],
 }
